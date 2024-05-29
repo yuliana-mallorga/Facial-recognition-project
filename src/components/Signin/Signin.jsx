bb1d243ccb76faf8b3709d40bffa14f14d6c7977
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-05-29 00:26:19 -0300

    Create onEmailChange, onPasswordChange, and onSubmitSignIn functions and add the useSatate hook in the Signin.jsx component. Install the axios dependency.

--- src/components/Signin/Signin.jsx
+++ src/components/Signin/Signin.jsx
@@ -1,8 +1,48 @@
 import PropTypes from "prop-types";
-
+import { useState } from "react";
+import axios from 'axios'
 function Signin({ onRouteChange }) {
+  
+  
+  const [signInEmail, setSignInEmail] = useState("");
+  const [signInPassword, setSignInPassword] = useState("");
+  
+  const onEmailChange = (event)=> {
+    return setSignInEmail(event.target.value)
+  }
+  
+  const onPasswordChange = (event) => {
+    return setSignInPassword(event.target.value)
+  }
+
+
+  const onSubmitSignIn = async (e) => {
+    e.preventDefault()
+    
+    try {
+      const r = await axios.post('http://localhost:4000/signin', {
+        headers: {
+          'Accept': 'application/json'
+        },
+        body:{
+          email: signInEmail,
+          password: signInPassword
+        }
+      })
+      const res = r.data
+      
+      if(res.success){
+        onRouteChange("home")
+      }else{
+        console.log('Usuario o contrase;a incorrecto.');
+      }
+    } catch (error) {
+      console.log('Usuario o contrase;a incorrecto.');
+    }
+  }
+  
   return (
-    <article className="br3 pa4 ba dark-gray b--black-10 mv4 w-100 w-50-m mw7 shadow-5 center">
+    <article className="br3 pa4 ba dark-gray b--black-10 mv5 w-100 w-50-m mw7 shadow-5 center">
       <main className="pa4 black-80">
         <form className="measure center">
           <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
@@ -16,6 +56,7 @@ function Signin({ onRouteChange }) {
                 type="email"
                 name="email-address"
                 id="email-address"
+                onChange = {onEmailChange}
               />
             </div>
             <div className="mv3">
@@ -27,13 +68,14 @@ function Signin({ onRouteChange }) {
                 type="password"
                 name="password"
                 id="password"
+                onChange = {onPasswordChange}
               />
             </div>
           </fieldset>
           <div>
             <div className="flex justify-center">
               <input
-                onClick={() => onRouteChange("home")}
+                onClick={onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Sign in"
