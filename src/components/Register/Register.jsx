85d9e631fc9fc5ab76619b42ab900862b8160487
Yuliana Mallorga <yulianamallorga@gmail.com>
2024-11-21 18:55:53 -0300

    enhance register component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import axios from 'axios';
 import Swal from 'sweetalert2';
 
-
 function Register({ onRouteChange, loadUser }) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -12,6 +11,7 @@ function Register({ onRouteChange, loadUser }) {
   const onNameChange = (event) => {
     return setName(event.target.value);
   };
+
   const onEmailChange = (event) => {
     return setEmail(event.target.value);
   };
@@ -22,8 +22,9 @@ function Register({ onRouteChange, loadUser }) {
 
   const onSubmitRegister = async (e) => {
     e.preventDefault()
+    
     try {
-      const res= await axios({
+      const res = await axios({
         method: 'post',
         url:`${import.meta.env.VITE_DATABASE_URL}/register`, 
         headers: {
@@ -35,31 +36,27 @@ function Register({ onRouteChange, loadUser }) {
           name: name,
         }
       })
-        if(res.data){  
-          loadUser(res.data);
-          onRouteChange("home");
-          Swal.fire({
-            title: "Your registration was successful!",
-            icon: "success"
-          });
-        }else{
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Complete all fields!"
-          });
-        }
+
+      if (!res.data) {
+        return fireAlert("Oops...", "error", "Complete all fields!")
+      }
       
+      loadUser(res.data);
+      onRouteChange("home");
+      fireAlert("Your registration was successful!", "success")
     } catch (error) {
-      Swal.fire({
-        icon: "error",
-        title: "Oops...",
-        text: "Did not register! This user probably exists"
-      });
-     
+      fireAlert("Did not register!", "error")
     }
   };
   
+  const fireAlert = (title, icon, text = null) => {
+    Swal.fire({
+      title,
+      icon,
+      text
+    });
+  }
+
   return (
     <article className="br3 pa4 ba dark-gray b--black-10 mv4 w-100 w-50-m mw7 shadow-5 center">
       <main className="pa4 black-80">
